2465fffa4ccc5f5f6b3f9b978ebc47a30622ab4d
Xiaojian Zheng <zhengxiaojian@apache.org>
2025-01-09 00:49:10 +0800

    init db

--- src/new_connection/new_connection.rs
+++ src/new_connection/new_connection.rs
@@ -1,5 +1,7 @@
+use leptos::attr::required;
 use crate::i18n::{t, use_i18n};
 use leptos::prelude::*;
+use leptos_i18n::__private::macros_reexport::t_string;
 use thaw::*;
 
 #[component]
@@ -11,7 +13,18 @@ pub fn NewConnection(open: RwSignal<bool>) -> impl IntoView {
             <DialogSurface>
                 <DialogBody>
                     <DialogTitle>{t!(i18n, new_connection)}</DialogTitle>
-                    <DialogContent>123</DialogContent>
+                    <DialogContent>
+                        <form>
+                            <FieldContextProvider>
+                                <Field label=t_string!(i18n, host) required=true>
+                                    <Input rules=vec![InputRule::required(true.into())]/>
+                                </Field>
+                                <Field label=t_string!(i18n, port) required=true>
+                                    <SpinButton<u32> step_page=1 max=65535/>
+                                </Field>
+                            </FieldContextProvider>
+                        </form>
+                    </DialogContent>
                     <DialogActions>
                         <Button appearance=ButtonAppearance::Primary>{t!(i18n, confirm)}</Button>
                         <Button on:click=move |_| open.set(false)>{t!(i18n, cancel)}</Button>
